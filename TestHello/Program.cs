2c839f6651da864952a41e03e9198590f5de3381
Raheem Chisman <raheem.chisman@gmail.com>
2022-11-16 11:59:39 -0500

    Add project files.

---
+++ TestHello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.WriteLine("Testline2");
+            Console.WriteLine("Hello Raheem");
+        }
+    }
+}
